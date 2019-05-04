67f00ba68fdde9f1f829b7f57efe98dc16eabb43
neshimbekova <neshimbekova@gmail.com>
2019-05-04 16:37:13 -0500

    fg

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,20 @@
-node {
-  properties([parameters([string(defaultValue: 'IP', description: 'Where to build?', name: 'ENV', trim: true)])])
-  stage("Clone repo"){
-    git 'git@github.com:neshimbekova/Flaskex.git'
-  }
-  stage('Build application') {
-    sh "ssh ec2-user@${ENV} sudo yum install python-pip -y"
-    sh "ssh ec2-user@${ENV} sudo rm -rf /tmp/*"
-  }
-  stage("Copy flask"){
-    sh "scp -r * ec2-user@${ENV}:/tmp"
-  }
-  stage("Create folder"){
-    sh "ssh ec2-user@${ENV} sudo mkdir -p /flaskex"
-  }
-  // Block to work on next
-  stage("Write to a file"){
-    sh "ssh ec2-user@${ENV} echo [Unit] > /tmp/flaskex.service"
+node{
+    properties([parameters([string(defaultValue: 'IP', description: 'Where to build e.g IP', name: 'ENV', trim: true)])])
+    stage("Clone repo"){
+        git 'git@github.com:farrukh90/Flaskex.git'
+    }
+    stage("Remove /tmp content"){
+        sh "ssh ec2-user@${ENV} sudo rm -rf /tmp/*"
+    }
+    stage("Copy filese over"){
+        sh "scp -r * ec2-user@${ENV}:/tmp"
+    }
+    stage("Create Folder"){
+        sh "ssh ec2-user@${ENV} sudo mkdir -p /flaskex"
+    }
+    //Block to work on next
+    stage("Write to a file"){
+        sh "ssh ec2-user@${ENV} echo [Unit] > /tmp/flaskex.service"
         sh "ssh ec2-user@${ENV} echo After=network.target >> /tmp/flaskex.service"
         sh "ssh ec2-user@${ENV} echo [Service] >> /tmp/flaskex.service"
         sh "ssh ec2-user@${ENV} echo Type=simple >> /tmp/flaskex.service"
@@ -25,21 +24,21 @@ node {
         sh "ssh ec2-user@${ENV} echo WantedBy=multi-user.target >> /tmp/flaskex.service"
     }
     //Block to work on next
-
-  stage("Copy to the system"){
-    sh "ssh ec2-user@${ENV} sudo cp -r /tmp/flaskex.service  /etc/systemd/system"
-  }
-  stage("move files from tmp to /flaskex"){
-    try {
-    sh "ssh ec2-user@${ENV} sudo cp -r /tmp/* /flaskex"
-  }
-    catch(err){
-      sh "echo did not copy"
+    stage("Copy to System"){
+        sh "ssh ec2-user@${ENV} sudo cp -r /tmp/flaskex.service /etc/systemd/system"
     }
-  stage("install requirements.txt"){
-    sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
-  }
-  stage("App Run"){
-    sh "ssh ec2-user@${ENV} systemctl star flaskex"
-  }
+    stage("move files to /flaskex"){
+        try {
+            sh "ssh ec2-user@${ENV} sudo cp -r /tmp/* /flaskex"
+        }
+        catch(err){
+            sh "echo did not copy"
+        }
+    }
+    stage("Install requiremennts"){
+        sh "ssh ec2-user@${ENV} sudo pip install -r /flaskex/requirements.txt"
+    }
+    stage("App Run"){
+        sh "ssh ec2-user@${ENV}  systemctl start flaskex"
+    }
 }
